90000c3caa20a9976672474a1806fd3fefd90c5b
Gerson Herrera <gersonhareb@hotmail.com>
2022-04-14 16:05:36 -0400

    Legendary first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}. Have a great day!`);
+});
+
+const login = [
+    {
+        mail:"gerson@technomancers.cl",
+        pass:"123",
+    }
+]
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+app.use("/bootstrap", express.static("node_modules/bootstrap/dist"));
+app.use("/css", express.static("public/css"));
+
+app.get("/", (_req, res) => {
+    res.sendFile(__dirname + "public/index.html");
+});
+
+// app.get("/login", (req, res) => {
+
+
+
+
+
